4abb1bffb6992b2466b08fcb053ed0ef48d5bfdb
ToMMaaY <thuynh629@gmail.com>
2019-04-03 12:39:06 -0700

    init react-app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="page-container">
+        <header className="header">
+          <h1>Hello</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
